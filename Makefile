dcfa013ffcf3b7608a220aafcb809151a6d6ceda
Forest Gregg <fgregg@uchicago.edu>
2021-06-03 11:00:24 -0400

    initial commit

---
+++ Makefile
@@ -0,0 +1,17 @@
+.PHONY : all
+all : 
+	$(MAKE) update_raw
+	$(MAKE) f7.csv
+
+f7.db : f7.csv
+	sqlite3 $@ -csv '.import $< f7'
+
+f7.csv : $(patsubst %.xlsx,%.csv,$(wildcard raw/*.xlsx))
+	python scripts/to_csv.py $^ | csvsort | uniq | csvcut -x  > $@
+
+%.csv : %.xlsx
+	in2csv $< | sed '/^Notice Date/,$$!d' > $@
+
+.PHONY : update_raw
+update_raw :
+	curl https://www.fmcs.gov/resources/documents-and-data/#tab-d3d7f5344cef9bab4d3 | grep Notices.xlsx | sed -n 's/.*href="\([^"]*\).*/\1/p' | wget -nd -i - -P raw -nc
---
+++ scripts/to_csv.py
@@ -0,0 +1,63 @@
+import sys
+import csv
+
+HEADER = ('notice_date', 'initiated_date', 'employer', 'employer_street',
+          'employer_city', 'employer_state', 'employer_zip',
+          'employer_representative', 'employer_representative_phone',
+          'employer_representative_title', 'employer_representative_email',
+          'union_name', 'union_street', 'union_city', 'union_state',
+          'union_zip', 'union_representative', 'union_representative_phone',
+          'union_representative_title', 'union_representative_email',
+          'affected_location_city', 'affected_location_state',
+          'affected_location_zip', 'expiration_date', 'naics',
+          'industry', 'bargaining_unit_size', 'establishment_size',
+          'notice_submitted_by', 'category', 'healthcare_related')
+
+HEADER_LOOKUP = {'e-street': 'employer_street',
+                 'e-city': 'employer_city',
+                 'e-state': 'employer_state',
+                 'e-zip': 'employer_zip',
+                 'employer_rep': 'employer_representative',
+                 'e-representative': 'employer_representative',
+                 'e-rep_phone': 'employer_representative_phone',
+                 'employer_rep_phone': 'employer_representative_phone',
+                 'e-rep_title': 'employer_representative_title',
+                 'employer_rep_title': 'employer_representative_title',
+                 'employer_rep_email': 'employer_representative_email',
+                 'union_name_&_local_number': 'union_name',
+                 'u-street': 'union_street',
+                 'u-city': 'union_city',
+                 'u-state': 'union_state',
+                 'u-zip': 'union_zip',
+                 'u-representative': 'union_representative',
+                 'union_rep': 'union_representative',
+                 'u-rep_title': 'union_representative_title',
+                 'union_rep_title': 'union_representative_title',
+                 'u-rep_phone': 'union_representative_phone',
+                 'union_rep_phone': 'union_representative_phone',
+                 'union_rep_email': 'union_representative_email',
+                 'a-city': 'affected_location_city',
+                 'a-state': 'affected_location_state',
+                 'a-zip': 'affected_location_zip',
+                 'notice_sumbitted_by': 'notice_submitted_by'}
+
+header_set = set(HEADER)
+header_set.update({'', 'e-street_2', 'u-street_2'})
+writer = csv.DictWriter(sys.stdout, fieldnames=HEADER, extrasaction='ignore')
+writer.writeheader()
+
+for filename in sys.argv[1:]:
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        slugged_fields = (field.lower().replace(' ', '_') for field in reader.fieldnames)
+        normalized_fields = [field if field in header_set else HEADER_LOOKUP[field]
+                             for field in slugged_fields]
+        assert set(normalized_fields) - header_set == set(), print(set(normalized_fields) - header_set)
+        reader.fieldnames = normalized_fields
+        for row in reader:
+            if 'e-street_2' in row:
+                row['employer_street'] += '\n' + row.pop('e-street_2')
+            if 'u-street_2' in row:
+                row['union_street'] += '\n' + row.pop('u-street_2')
+                
+            writer.writerow(row)
